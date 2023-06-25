5d9531a75cce969fc3f6a243e259b71c7745edb4
ArnauBarbaUdG <125899448+ArnauBarbaUdG@users.noreply.github.com>
2023-06-25 20:07:14 +0200

    Update game_controller.js

--- t_individual/js/game_controller.js
+++ t_individual/js/game_controller.js
@@ -1,57 +1,58 @@
-var gameObj = function (){
+var gameObj = function () {
 	// Definició de les imatges
 	const back = "../resources/back.png";
-	const items = ["../resources/cb.png","../resources/co.png","../resources/sb.png",
-	"../resources/so.png","../resources/tb.png","../resources/to.png"];
+	const items = ["../resources/cb.png", "../resources/co.png", "../resources/sb.png", "../resources/so.png", "../resources/tb.png", "../resources/to.png"];
+
 	// Comprovació de si hi ha una partida guardada
 	let l_partida = null;
-	if (sessionStorage.idPartida && localStorage.partides){
+	if (sessionStorage.idPartida && localStorage.partides) {
 		let arrayPartides = JSON.parse(localStorage.partides);
 		if (sessionStorage.idPartida < arrayPartides.length)
 			l_partida = arrayPartides[sessionStorage.idPartida];
 	}
+
 	var vueInstance = new Vue({
 		el: "#game_id",
 		data: {
-			username:'',
+			username: '',
 			current_card: [],
 			items: [],
 			num_cards: 2,
 			bad_clicks: 0
 		},
-		created: function(){
+		created: function () {
 			// Si hi ha una partida guardada, es carreguen les dades
-			if (l_partida){
+			if (l_partida) {
 				this.username = l_partida.username;
 				this.current_card = l_partida.current_card;
 				this.items = l_partida.items;
 				this.num_cards = l_partida.num_cards;
 				this.bad_clicks = l_partida.bad_clicks;
 			}
-			else{
+			else {
 				// Si no hi ha una partida guardada, es carreguen les opcions per defecte
-				this.username = sessionStorage.getItem("username","unknown");
+				this.username = sessionStorage.getItem("username", "unknown");
 				// Es seleccionen les cartes a mostrar i es barregen
-				this.items = items.slice(); 
-				this.items.sort(function(){return Math.random() - 0.5}); 
+				this.items = items.slice();
+				this.items.sort(function () { return Math.random() - 0.5 });
 				this.items = this.items.slice(0, this.num_cards);
 				// Es duplica l'array de cartes i es torna a barrejar
-				this.items = this.items.concat(this.items); 
-				this.items.sort(function(){return Math.random() - 0.5}); 
-				for (var i = 0; i < this.items.length; i++){
-					this.current_card.push({done: false, texture: back});
+				this.items = this.items.concat(this.items);
+				this.items.sort(function () { return Math.random() - 0.5 });
+				for (var i = 0; i < this.items.length; i++) {
+					this.current_card.push({ done: false, texture: back });
 				}
 			}
 			sessionStorage.clear();
 		},
 		methods: {
 			// Mètode per mostrar la carta seleccionada
-			clickCard: function(i){
+			clickCard: function (i) {
 				if (!this.current_card[i].done && this.current_card[i].texture === back)
-					Vue.set(this.current_card, i, {done: false, texture: this.items[i]});
+					Vue.set(this.current_card, i, { done: false, texture: this.items[i] });
 			},
 			// Mètode per guardar la partida al servidor
-			save: function(){
+			save: function () {
 				fetch("../php/save.php", {
 					method: "POST",
 					body: JSON.stringify({
@@ -65,13 +66,13 @@ var gameObj = function (){
 					alert(json.score);
 					this.local_save();
 				})
-				.catch((error) => {
-					alert('Error:', error);
-					this.local_save();
-				});
+					.catch((error) => {
+						alert('Error:', error);
+						this.local_save();
+					});
 			},
 			// Mètode per guardar la partida localment
-			local_save: function(){
+			local_save: function () {
 				let partida = {
 					username: this.username,
 					current_card: this.current_card,
@@ -80,9 +81,9 @@ var gameObj = function (){
 					bad_clicks: this.bad_clicks
 				}
 				let arrayPartides = [];
-				if(localStorage.partides){
+				if (localStorage.partides) {
 					arrayPartides = JSON.parse(localStorage.partides);
-					if(!Array.isArray(arrayPartides)) arrayPartides = [];
+					if (!Array.isArray(arrayPartides)) arrayPartides = [];
 				}
 				arrayPartides.push(partida);
 				localStorage.partides = JSON.stringify(arrayPartides);
@@ -91,42 +92,39 @@ var gameObj = function (){
 		},
 		watch: {
 			// Es vigila si les cartes seleccionades coincideixen
-			current_card: function(value){
+			current_card: function (value) {
 				if (value.texture === back) return;
 				var front = null;
 				var i_front = -1;
-				for (var i = 0; i < this.current_card.length; i++){
-					if (!this.current_card[i].done && this.current_card[i].texture !== back){
-						if (front){
-							if (front.texture === this.current_card[i].texture){
+				for (var i = 0; i < this.current_card.length; i++) {
+					if (!this.current_card[i].done && this.current_card[i].texture !== back) {
+						if (front) {
+							if (front.texture === this.current_card[i].texture) {
 								front.done = this.current_card[i].done = true;
 								this.num_cards--;
 							}
-							else{
-								Vue.set(this.current_card, i, {done: false, texture: back});
-								Vue.set(this.current_card, i_front, {done: false, texture: back});
+							else {
+								Vue.set(this.current_card, i, { done: false, texture: back });
+								Vue.set(this.current_card, i_front, { done: false, texture: back });
 								this.bad_clicks++;
 								break;
 							}
 						}
-						else{
+						else {
 							front = this.current_card[i];
 							i_front = i;
 						}
 					}
-				}			
+				}
 			}
 		},
 		computed: {
 			// Es calcula la puntuació actual
-			score_text: function(){
+			score_text: function () {
 				return 100 - this.bad_clicks * 20;
 			}
 		}
 	});
+
 	return {};
 }();
-
-
-
-
